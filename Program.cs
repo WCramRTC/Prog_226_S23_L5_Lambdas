dfc9aa6d071835557730337ad71bb4b766bef836
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-17 17:28:47 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Prog_226_S23_L5_Lambdas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambdas
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
