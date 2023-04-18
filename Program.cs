89c761f60e8157078d274db4309dca810aab1673
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-18 13:30:13 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,25 @@
         {
             // Lambdas
             Console.WriteLine("Hello, World!");
+
+            List<int> numbers = new List<int>() { 1, 25, 3, 1, 5 };
+
+            var findAllGreater = numbers.FindAll(x => x > 2);
+
+            var displayList = (List<int> list) =>
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            };
+
+            displayList(findAllGreater);
+
+        // Links
+        //https://www.csharptutorial.net/csharp-tutorial/csharp-lambda-expression/ 
+        //https://www.c-sharpcorner.com/UploadFile/bd6c67/lambda-expressions-in-C-Sharp/
+
         }  // Main
 
     } // class
